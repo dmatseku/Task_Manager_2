2bd5878a1f608a3438f81c9308a943ec9495cfa4
dmatseku <ZNbig2A3>
2020-07-08 15:48:04 +0300

    removed unnecessary scss file

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -19,7 +19,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/authorize.css') }}" rel="stylesheet">
 </head>
 <body>
 <div id="auth">
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,8 +16,6 @@ mix.js('resources/js/app.js', 'public/js')
 
 mix.sass('resources/sass/auth/auth.scss', 'public/css');
 
-mix.sass('resources/sass/auth/authorize.scss', 'public/css');
-
 mix.js('resources/js/task/list.js', 'public/js')
     .sass('resources/sass/task/list.scss', 'public/css');
 
